e1ee40f4dcf857ac9914c3bbfc470dde0cca85dd
王振宁 <wangzhenning@wangzhenningdeMacBook-Pro.local>
2015-08-25 16:31:43 +0800

    创建一个新的工程

---
+++ GitTest/GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by 王振宁 on 15/8/25.
+//  Copyright (c) 2015年 王振宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitTest/GitTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitTest
+//
+//  Created by 王振宁 on 15/8/25.
+//  Copyright (c) 2015年 王振宁. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
